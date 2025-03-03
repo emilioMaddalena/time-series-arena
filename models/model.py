5b942c8059f1760c3c5a23213d186613a6f3b1d7
emilioMaddalena <emilio.tanowe.maddalena@gmail.com>
2025-03-03 07:53:01 +0100

    Rectify the base class
    - Now the class doesn't own the test data anymore (sketchy)

--- models/model.py
+++ models/model.py
@@ -6,31 +6,25 @@ import numpy as np
 class TimeSeriesModel(ABC):
     """A generic class that represents a time series model.
 
-    N.B. The model object owns the train and test data.
+    N.B. The model object only owns the train data.
     """
 
-    def __init__(self, train: np.ndarray, test: np.ndarray):
-        """Store the training and test data."""
-        self.train = train
-        self.test = test
+    def __init__(self, train_series: np.ndarray):
+        """Store the training data."""
+        self.train_series = train_series
         self.model = None
 
     @abstractmethod
-    def learn_model(self, train_data: np.ndarray, **kwargs):
-        """Learn a model based on some training data and hyperparameters search."""
+    def learn_model(self, **kwargs):
+        """Learn a model based on the training data (attribute) and hyperparameters."""
         pass
 
     @abstractmethod
-    def predict_training_set(self, frac: float) -> np.ndarray:
-        """Predict the last frac % of the training time series (in-sample)."""
+    def predict_training_set(self):
+        """Predict on the training set (in-sample)."""
         pass
 
     @abstractmethod
-    def predict_test_set(self, frac: float) -> np.ndarray:
-        """Predict the last frac % of the test time series (out-of-sample)."""
-        pass
-
-    @abstractmethod
-    def predict(self, context: np.ndarray, steps: int) -> np.ndarray:
-        """Predict (generic) N steps ahead given some tim-series context."""
+    def predict_test_set(self, test_series: np.ndarray) -> np.ndarray:
+        """Predict on the test set (out-of-sample)."""
         pass
